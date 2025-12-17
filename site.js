e8619bcd0e1f87b480af2feff0ca080ddcc1115c
Admin <bidsniper@snipeable.com>
2025-12-17 12:37:16 -0800

    update

--- site.js
+++ site.js
@@ -9,5 +9,21 @@ function copyToClipboard(element) {
     document.execCommand('copy');
     document.body.removeChild(temp);
 
+    // Google Analytics event
+    if (typeof gtag !== 'undefined')
+        gtag('event', 'copy_install_command', {
+            'event_category': 'Downloads',
+            'event_label': element.includes('windows') ? 'Windows' : 'Mac'
+        });
+
     alert('Copied to clipboard!');
+}
+
+// Track download intent
+function trackDownload(source) {
+    if (typeof gtag !== 'undefined')
+        gtag('event', 'download_intent', {
+            'event_category': 'Downloads',
+            'event_label': source
+        });
 }
